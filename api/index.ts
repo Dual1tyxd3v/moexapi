3af7634499f139dca6d044a09ae8f5b4331ef0d7
Dual1tyxd3v <d3vilc0re@gmail.com>
2024-05-28 22:02:34 +0700

    Added noobclub api

--- api/index.ts
+++ api/index.ts
@@ -1,15 +1,15 @@
+import { parseNClub } from '../utils';
+
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
-const jsdom = require('jsdom');
+const { parsePob, parseFilms } = require('../utils');
 const port = 3002;
 const app = express();
 
 app.use(cors());
 app.use(bodyParser.json());
 
-const { JSDOM } = jsdom;
-const POEWIKI_URL = 'https://www.poewiki.net/wiki/';
 const NG_OPT_URL =
   'https://iss.moex.com/iss/engines/futures/markets/forts/securities/NGH4.jsonp?iss.meta=off&iss.json=extended&callback=JSON_CALLBACK&lang=ru&contractname=1';
 
@@ -40,6 +40,12 @@ app.post('/films', async (req, res) => {
   if (!url) res.send({ data: null, error: 'Broken URL' });
 
   const data = await parseFilms(url);
+
+  res.send(JSON.stringify(data));
+});
+
+app.get('/nclub', async (_, res) => {
+  const data = await parseNClub();
 
   res.send(JSON.stringify(data));
 });
@@ -79,92 +85,5 @@ async function getOptions() {
 app.listen(port, (error) => {
   console.log(`Server listening on port ${port}`);
 });
-
-async function parsePob(url) {
-  try {
-    const formatedUrl = url.replace('/ru/', '/us/');
-    const splittedUrl = url.split('/');
-    const itemName = splittedUrl[splittedUrl.length - 1];
-
-    const imageResp = await fetch(`${POEWIKI_URL}${itemName}`);
-    if (!imageResp.ok) throw new Error('Cant load URL');
-
-    const imageHtml = await imageResp.text();
-    const imageDom = new JSDOM(imageHtml);
-    const imageDoc = imageDom.window.document;
-
-    const prefixUrl = 'https://www.poewiki.net';
-    const imageSrc = imageDoc.querySelector('.images').querySelector('img').src.split('.png')[0] + '.png';
-
-    const resp = await fetch(formatedUrl);
-    if (!resp.ok) throw new Error('Cant load URL');
-
-    const html = await resp.text();
-    const dom = new JSDOM(html);
-    const doc = dom.window.document;
-
-    const header = doc.querySelector('.itemHeader');
-    const name = [...header.querySelectorAll('.itemName')].map((div) => div.textContent);
-
-    const level = +doc.querySelector('.requirements').querySelector('span').textContent || -1;
-
-    const implicit = doc.querySelector('.implicitMod')?.textContent || '---';
-
-    const regex = /(<([^>]+)>)/gi;
-    const explicit = doc
-      .querySelector('.explicitMod')
-      .innerHTML.split('<br>')
-      .map((str) => str.replace(regex, ''));
-
-    const text = doc.querySelector('.FlavourText').innerHTML.split('<br>').join('\n');
-
-    const drops = imageDoc.querySelector('#Item_acquisition').closest('h2').nextSibling;
-    const source = drops.querySelector('.mw-redirect') ? drops.querySelector('.mw-redirect').textContent : 'world drop';
-
-    return { data: { name, level, implicit, explicit, text, image: `${prefixUrl}${imageSrc}`, source }, error: '' };
-  } catch (e) {
-    console.log(e);
-    return { data: null, error: 'Cant load URL' };
-  }
-}
-
-async function parseFilms(url) {
-  try {
-    const resp = await fetch(url);
-    if (!resp.ok) {
-      console.log('Cant load URL', resp);
-      return { data: resp };
-      return { data: null, error: 'Cant load url' };
-    }
-
-    const html = await resp.text();
-    const dom = new JSDOM(html);
-    const doc = dom.window.document;
-
-    const [films, , , newSerials, newSeasons] = [...doc.querySelectorAll('.box4')];
-    const data = [films, newSerials, newSeasons].map((block) => {
-      return parseFilmDetails(block);
-    });
-
-    return { data, error: null };
-  } catch (e) {
-    console.log(e);
-    return { data: null, error: 'Something went wrong :(' };
-  }
-}
-function parseFilmDetails(filmDiv) {
-  return [...filmDiv.querySelectorAll('.short')].map((film) => {
-    const imgSrc = film.querySelector('img').dataset.srcset.replace(' 190w', '');
-    const title = film.querySelector('a').textContent;
-
-    const src = film.querySelector('a').href.slice(film.querySelector('a').href.lastIndexOf('/'));
-
-    return {
-      imgSrc,
-      title,
-      src,
-    };
-  });
-}
 
 module.exports = app;
