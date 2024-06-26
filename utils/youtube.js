e50930f428f66a0960345147e8a8146be86b7209
Dual1tyxd3v <d3vilc0re@gmail.com>
2024-06-26 13:52:11 +0700

    Fix

--- utils/youtube.js
+++ utils/youtube.js
@@ -2,16 +2,32 @@ const jsdom = require('jsdom');
 const { JSDOM } = jsdom;
 const puppeteer = require('puppeteer');
 
+const chromium = require('@sparticuz/chromium');
+const puppeteerCore = require('puppeteer-core');
+
 const EMBED_URL = 'https://www.youtube.com/embed/';
 
 const getVideo = async (url) => {
   try {
-    const browser = await puppeteer.launch();
+    let browser = await puppeteerCore.launch({
+      args: chromium.args,
+      defaultViewport: chromium.defaultViewport,
+      executablePath: await chromium.executablePath(),
+      headless: chromium.headless,
+    });
+    if (process.env.NODE_ENV === 'development') {
+      browser = await puppeteer.launch({
+        args: ['--no-sandbox', '--disable-setuid-sandbox'],
+        headless: true,
+      });
+    }
+
     const page = await browser.newPage();
 
     await page.goto(url);
 
     const html = await page.content();
+    await browser.close();
     const dom = new JSDOM(html);
     const doc = dom.window.document;
 
